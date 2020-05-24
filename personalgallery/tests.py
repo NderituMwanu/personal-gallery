0109c94cbea431023e61012d629fbab49e950052
NderituMwanu <mwanugerald@gmail.com>
2020-05-25 01:28:27 +0300

    test cases for each model

--- personalgallery/tests.py
+++ personalgallery/tests.py
@@ -1,6 +1,39 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from django.core.files.uploadedfile import SimpleUploadedFile
 
 from django.test import TestCase
+from .models import Editor, Image, Location, Category
 
-# Create your tests here.
+# All view function tests
+
+class EditorTestClass(TestCase):
+    #Set Up Method
+    def setUp(self):
+        self.gerald= Editor(Username = 'Gerald', email = "samplemail@gmail.com")
+
+    #testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.gerald,Editor))
+
+class ImageTestClass(TestCase):
+
+    # def setUp(self):
+    #     self.newphoto= Image(image_name='test_image.jpg', image_description = "bob", pub_date = "12-3-2020") 
+
+    def setUp(self):
+
+        #Posting a new image and saving it
+        self.newphoto = Image(image_name ="test.jpg", image_description = "abcd", pub_date = "12-3-2020")
+        self.newphoto.save_image()
+
+
+        #deletion of an image
+        self.newphoto = Image(image_name ="test.jpg", image_description = "abcd", pub_date = "12-3-2020")
+        self.newphoto.delete_image()
+
+
+
+    def test_get_image_by_id(self):
+        image_id = Image.post_of_today()
+        self.assertTrue(len(today_news) > 0) 
