f55bc6be71ee60aab4588ce889f65930cb6b1cb7
NderituMwanu <mwanugerald@gmail.com>
2020-05-25 01:27:20 +0300

    administrator model registries

--- personalgallery/admin.py
+++ personalgallery/admin.py
@@ -2,5 +2,11 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import Editor, Image, Location, Category
 
 # Register your models here.
+
+admin.site.register(Editor)
+admin.site.register(Image)
+admin.site.register(Location)
+admin.site.register(Category)
