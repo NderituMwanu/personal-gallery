dbc481183f1efba341dfd341bd019a4d6b40e1ad
NderituMwanu <mwanugerald@gmail.com>
2020-05-27 02:31:42 +0300

    application views

--- personalgallery/views.py
+++ personalgallery/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 import datetime as dt
 from django.shortcuts import render,redirect
-
+from .models import Image, Category, Location
 
 #my views
 # def welcome(request):
@@ -26,7 +26,8 @@ def post_time(request):
 
 def post_of_today(request):
     date = dt.date.today()
-    return render(request, 'all-pictures/todays-post.html', {"date": date,})
+    post = Image.post_of_today()
+    return render(request, 'all-pictures/todays-post.html', {"date": date, "post":post})
 
 def past_day_post(request,past_date):
         # Converts data from the string Url
@@ -43,7 +44,42 @@ def past_day_post(request,past_date):
 
     if date == dt.date.today():
         return redirect(post_of_day)
-            
+
+    post = Image.post_of_day(date)         
     return render(request, 'all-pictures/past-post.html', {"date": date})
 
     
+def search_results(request):
+    if 'image' in request.GET and request.GET["image"]:
+        search_term = request.GET.get("article")
+        searched_images = Article.search_by_location(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'all-pictures/search.html',{"message":message,"images": searched_images})
+
+    else:
+        message = "Sorry You haven't searched for any term"
+        return render(request, 'all-pictures/search.html',{"message":message})
+
+
+def location(request,location_id):
+    gallery = Image.objects.filter(location__name=location_id)
+    location = Location.objects.all()
+    category= Category.objects.all()
+
+    return render(request,"all-pictures/images.html", {"gallery":gallery,"location":location,'category':category})
+
+
+def image(request,image_id):
+    print('/images')
+    try:
+        image = Image.objects.get(id = image_id)
+    except DoesNotExist:
+        raise Http404()
+    return render(request, "all-pictures/image.html", {"image":image})  
+
+def category_image(request,category_id):
+    images=Image.objects.filter(category__name=category_id)
+    category= Categorys.objects.all()
+    location= Location.objects.all()
+    return render(request,'all-pictures/image.html',{'gallery':images,'category':category,'location':location})
